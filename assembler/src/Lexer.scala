d9d82dd5c4e40e46e7dcaea03cd69684631f44d9
MrBocch <jorgealberto1436@gmail.com>
2025-10-27 18:23:02 -0600

    working

--- assembler/src/Lexer.scala
+++ assembler/src/Lexer.scala
@@ -1,15 +1,18 @@
 import scala.util.Try
 
 object Lexer:
+    // need to handle comments
+    // also handle "strings"
     def lexemes(source : String) : Vector[String] =
-        def loop(s : Vector[Char], result : Vector[String], carry : String = "") : Vector[String] =
+        def loop(s : Vector[Char], result : Vector[String], carry : String = "", comment : Boolean = false) : Vector[String] =
             val head : Option[Char] = s.headOption
             val tail : Option[Vector[Char]] = Try {s.tail}.toOption
             (head, tail) match {
                 case (Some(head), Some(tail)) => {
+                    println(s"Head: $head")
                     head match {
-                        case ' ' | '\n' => loop(tail, result.appended(carry), "")
-                        case ',' | ':' => loop(tail, result.appended(carry).appended(head.toString), "")
+                        case ' ' | '\n' | '\t' => loop(tail, result.appended(carry), "")
+                        case ',' | ':' | '[' | ']' | '-' | '+' | '#' => loop(tail, result.appended(carry).appended(head.toString), "")
                         case _ => loop(tail, result, carry+head.toString)
                     }
                 }
@@ -18,10 +21,7 @@ object Lexer:
                     println(s"result: $result carry: $carry")
                     result
                 }
-                case _ => {
-                    println("gest to this case")
-                    result
-                }
+                case _ => result.appended(carry.toString)
             }
 
         loop(source.trim.toVector, Vector[String]())
--- assembler/src/Main.scala
+++ assembler/src/Main.scala
@@ -22,6 +22,6 @@ object Main:
         println(file)
         val source : Option[String] = readFile(file)
         source match {
-            case Some(s) => Lexer.lexemes(s).filter(_!="").foreach(println)
+            case Some(s) => Lexer.lexemes(s).filter(_!=" ").filter(!_.isEmpty).foreach((s:String)=> println(s"'$s'"))
             case _ => println("Failed to open file")
         }
