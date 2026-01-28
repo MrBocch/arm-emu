d9c828ad16a484d8db93148142574e0d541932d0
MrBocch <jorgealberto1436@gmail.com>
2026-01-28 10:42:56 -0600

    clean up

--- cmd/assembler/assembler.go
+++ cmd/assembler/assembler.go
@@ -7,13 +7,16 @@ import (
 // only support ascii
 func Lex(code string) []string {
 	// too bad golang has no sumtypes
-	// normal
-	// comment
+	// (normal | comment)
 	state := "normal"
 	// nested := 0
 	var lexemes []string
     for i := 0; i < len(code); i++ {
       	byte := code[i]
+
+       	if state == "comment" && byte == '\n' { state = "normal"; continue }
+        if state == "comment" { continue }
+
       	switch byte {
        	case ';': state = "comment"; continue
         case '/':
@@ -23,7 +26,6 @@ func Lex(code string) []string {
           	if code[i+1] == '/' { state = "comment"; continue }
        	}
 
-        if state == "comment" && byte == '\n' { state = "normal"; continue }
         if state != "comment" {
 	        fmt.Printf("Index %d: byte=%d char=%c\n", i, byte, byte)
         }
