61a3d3dd1fdd9474175c9a434fca8fc94e1efb39
MrBocch <jorgealberto1436@gmail.com>
2026-01-29 11:45:24 -0600

    comments actually work, prettier printing

--- cmd/assembler/assembler.go
+++ cmd/assembler/assembler.go
@@ -9,33 +9,50 @@ func Lex(code string) []string {
 	// too bad golang has no sumtypes
 	// (normal | comment)
 	state := "normal"
-	// nested := 0
+	old_line := 0
+	line := 1
 	var lexemes []string
+
     for i := 0; i < len(code); i++ {
+    	if old_line != line {
+    		fmt.Printf("\n")
+    		fmt.Printf("[%03d]: ", line)
+    		old_line = line 
+    	}
 		if state == "comment" {
+			start := line
 			for j := i +1; j < len(code)-1; j++ {
+				if code[j] == '\n' { line += 1 }
 				if code[j] == '*' && code[j+1] == '/' {
 					i = j + 2 
 					state = "normal"
 					continue 
 				}
 			}
+			if state != "normal" { panic(fmt.Sprintf("[%d]: unclosed comment", start)) }
 		} 
 		
       	byte := code[i]
       	switch byte {
+      	case '\n':
+      		line += 1
        	case ';':
-       	    for j := i+1; code[j] != '\n' && j < len(code); j++ { i = j } ; continue 
+       	    for j := i+1; code[j] != '\n' && j < len(code); j++ {
+       	    	i = j
+       	    }
         case '/':
-        	if i + 1 >= len(code) { panic("unexpected token '/'") } // should have actual system for reporting errors 
-          	if code[i+1] == '/' { for j := i+1; code[j] != '\n' && j < len(code); j++ { i = j } ; continue }
+        	if i + 1 >= len(code) { panic("error '/''") } // should have actual system for reporting errors 
+          	if code[i+1] == '/' {
+          		for j := i+1; code[j] != '\n' && j < len(code); j++ { i = j }
+          		continue 
+          	}
           	if code[i+1] == '*' { state = "comment"; continue }
-          	panic("unexpected token '/'")
-       	}
+          	panic(fmt.Sprintf("[%d]: unexpected token '/'", line))
 
-       	if state != "" {
-		    fmt.Printf("Index %d: byte=%d char=%c\n", i, byte, byte)
+		default:
+       		fmt.Printf("%c", byte)
        	}
+       	
     }
 
 	return lexemes
--- examples/test.asm
+++ examples/test.asm
@@ -1,9 +1,14 @@
 // this is a comment
 ;;  😁 this is also a comment
 add r1, r1, #3
+// need to add logic making sure
+// someone doesn't try to nest comments
 
-/*
- * Do we support
- * multiline comments
- * now?
- */ 
+/* comments
+ * now
+ * things
+ * are working
+ * they way they supposed to
+ *
+ // yup
+*/
