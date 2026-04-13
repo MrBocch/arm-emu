e29ae572936166f56964684a1a87ba92c171019e
MrBocch <jorgealberto1436@gmail.com>
2026-04-13 17:50:14 -0600

    fix issues with gui

--- cmd/vm/gui.go
+++ cmd/vm/gui.go
@@ -41,7 +41,7 @@ func RunGui(mem []uint32) {
 
 
 	a := app.New()
-	a.Settings().SetTheme(theme.LightTheme())
+	// a.Settings().SetTheme(theme.LightTheme())
 
 	w := a.NewWindow(APP_TITLE)
 	w.Resize(fyne.NewSize(700, 700))
@@ -54,6 +54,7 @@ func RunGui(mem []uint32) {
 }
 
 func makeLeft() fyne.CanvasObject {
+	R0Label  = widget.NewLabel("R0:  0x00000000")
 	R1Label  = widget.NewLabel("R1:  0x00000000")
 	R2Label  = widget.NewLabel("R2:  0x00000000")
 	R3Label  = widget.NewLabel("R3:  0x00000000")
@@ -77,7 +78,7 @@ func makeLeft() fyne.CanvasObject {
 		title,
 		nil, nil, nil,
 		container.NewVBox(
-			R1Label, R2Label, R3Label, R4Label,
+			R0Label, R1Label, R2Label, R3Label, R4Label,
 			R5Label, R6Label, R7Label, R8Label,
 			R9Label, R10Label, R11Label, R12Label,
 			LRLabel, SPLabel, PCLabel,
@@ -86,18 +87,18 @@ func makeLeft() fyne.CanvasObject {
 }
 
 func updateRegisters() {
-	R1Label.SetText(fmt.Sprintf("R0:  0x%08X", vm.registers[0]))
-	R2Label.SetText(fmt.Sprintf("R1:  0x%08X", vm.registers[1]))
-	R3Label.SetText(fmt.Sprintf("R2:  0x%08X", vm.registers[2]))
-	R4Label.SetText(fmt.Sprintf("R3:  0x%08X", vm.registers[3]))
-	R5Label.SetText(fmt.Sprintf("R4:  0x%08X", vm.registers[4]))
-	R6Label.SetText(fmt.Sprintf("R5:  0x%08X", vm.registers[5]))
-	R7Label.SetText(fmt.Sprintf("R6:  0x%08X", vm.registers[6]))
-	R8Label.SetText(fmt.Sprintf("R7:  0x%08X", vm.registers[7]))
-	R9Label.SetText(fmt.Sprintf("R8:  0x%08X", vm.registers[8]))
-	R10Label.SetText(fmt.Sprintf("R9: 0x%08X", vm.registers[9]))
-	R11Label.SetText(fmt.Sprintf("R10: 0x%08X", vm.registers[10]))
-	R12Label.SetText(fmt.Sprintf("R11: 0x%08X", vm.registers[11]))
+	R0Label.SetText(fmt.Sprintf("R0:  0x%08X", vm.registers[0]))
+	R1Label.SetText(fmt.Sprintf("R1:  0x%08X", vm.registers[1]))
+	R2Label.SetText(fmt.Sprintf("R2:  0x%08X", vm.registers[2]))
+	R3Label.SetText(fmt.Sprintf("R3:  0x%08X", vm.registers[3]))
+	R4Label.SetText(fmt.Sprintf("R4:  0x%08X", vm.registers[4]))
+	R5Label.SetText(fmt.Sprintf("R5:  0x%08X", vm.registers[5]))
+	R6Label.SetText(fmt.Sprintf("R6:  0x%08X", vm.registers[6]))
+	R7Label.SetText(fmt.Sprintf("R7:  0x%08X", vm.registers[7]))
+	R8Label.SetText(fmt.Sprintf("R8:  0x%08X", vm.registers[8]))
+	R9Label.SetText(fmt.Sprintf("R9: 0x%08X", vm.registers[9]))
+	R10Label.SetText(fmt.Sprintf("R10: 0x%08X", vm.registers[10]))
+	R11Label.SetText(fmt.Sprintf("R11: 0x%08X", vm.registers[11]))
 	R12Label.SetText(fmt.Sprintf("R12: 0x%08X", vm.registers[12]))
 	LRLabel.SetText(fmt.Sprintf("LR:  0x%08X", vm.registers[13]))
 	SPLabel.SetText(fmt.Sprintf("SP:  0x%08X", vm.registers[14]))
--- examples/mov.asm
+++ examples/mov.asm
@@ -1,11 +1,19 @@
-mov r3, #0b1
-mov r4, #2
-mov r5, #0x3
-
-mov r12, #999
-mov r0, r12
-mov r1, r12
-mov pc, #0
+mov r0, #0b01
+mov r1, #0x002
+mov r2, #3
+mov r3, #4
+mov r4, #5
+mov r5, #6
+mov r6, #7
+mov r7, #8
+mov r8, #9
+mov r9, #10
+mov r10, #11
+mov r11, #12
+mov r12, #13
+mov LR, #14
+mov SP, #15
+mov PC, #0
 halt
 // incorrect
-//mov r0, [address]
+// mov r0, [address]
